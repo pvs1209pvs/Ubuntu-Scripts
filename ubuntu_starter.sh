8df6d315e9da8c1888e51e5e4d167eebd51cf42b
Paramvir Singh <ps17cw@brocku.ca>
2020-11-18 14:56:30 -0500

    init commit

---
+++ ubuntu_starter.sh
@@ -0,0 +1,13 @@
+sudo apt purge firefox rhythmbox thunderbird
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
+sudo apt install git
+
+sudo snap install --classic code 
+sudo snap install --classic atom
+
+sudo snap install --classic intellij-idea-ultimate 
+sudo snap install --classic clion
