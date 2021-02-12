1bd00c8cff571f12d5d812ee7ce16795e12d161d
Paramvir <ps17cw@brocku.ca>
2021-02-11 21:48:31 -0500

    yes silent prompt added

--- ubuntu_starter.sh
+++ ubuntu_starter.sh
@@ -4,10 +4,10 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
 
-sudo apt install git
-sudo apt install cmake
-sudo apt install g++
-sudo apt install valgrind
+sudo apt install -y git
+sudo apt install -y cmake
+sudo apt install -y g++
+sudo apt install -y valgrind
 
 sudo snap install --classic code 
 sudo snap install --classic atom
