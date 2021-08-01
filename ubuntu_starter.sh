ae3f0cd11ef2843f4b8e72a85df5c7c9a4214e71
Paramvir Singh <ps17cw@brocku.ca>
2021-08-01 03:09:53 -0400

    gnome-tweak and notion install added

--- ubuntu_starter.sh
+++ ubuntu_starter.sh
@@ -1,16 +1,13 @@
-sudo apt purge firefox rhythmbox thunderbird
+sudo apt purge firefox rhythmbox thunderbird;
 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb;
+sudo dpkg -i google-chrome-stable_current_amd64.deb;
+rm google-chrome-stable_current_amd64.deb;
 
-sudo apt install -y git
-sudo apt install -y cmake
-sudo apt install -y g++
-sudo apt install -y valgrind
+sudo apt install -y git cmake g++ valgrind gnome-tweak-tool;
 
-sudo snap install --classic code 
-sudo snap install --classic atom
+sudo snap install --classic code;
+sudo snap install --classic intellij-idea-ulitmate;
+sudo snap install --classic clion;
 
-sudo snap install --classic intellij-idea-ultimate 
-sudo snap install --classic clion
+sudo snap install notion-snap;
